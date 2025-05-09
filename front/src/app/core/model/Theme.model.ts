1d586c922f305a8ab5dd49f0d1d06e2abde77ae3
Diengsalla1990 <diengdev66@gmail.com>
2025-05-09 19:25:28 +0200

    changer front par angular 19

--- front/src/app/core/model/Theme.model.ts
+++
@@ -1,9 +0,0 @@
-import { Article } from './Article.model';
-export class Theme {
-    constructor(
-      public id: number,
-      public name: string,
-      public description: string,
-      public article?: Article[]
-    ) {}
-  }
